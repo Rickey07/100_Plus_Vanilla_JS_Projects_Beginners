efdc7269977e5b4b51c98fa41f0f9d3b666dd46d
Rickey07 <Rickeyrickey747@gmail.com>
2022-09-14 22:24:39 +0530

    Initial Project Setup done!
    Project added // Styling yet to be added!

---
+++ Digital-Clock-JS/index.js
@@ -0,0 +1,45 @@
+const toggleButton = document.querySelector('.toggleButton');
+const hoursEle = document.querySelector('#hours');
+const minutesEle = document.querySelector('#minutes');
+const secondsEle = document.querySelector('#seconds');
+const statusEle = document.querySelector('#status')
+
+
+toggleButton.addEventListener('click' , () => {
+    document.body.classList.toggle('dark-mode')
+})
+
+
+let now = new Date;
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+
+const updateTime = (hours , minutes , seconds) => {
+    setInterval(() => {
+        if (seconds>=60) {
+            seconds = 0;
+            minutes++
+            if(minutes>=60) {
+                minutes = 0;
+                hours++;
+                if (hours>23) {
+                    hours = 0;
+                }
+            }
+        }
+        renderTime(hours,minutes,seconds)
+        seconds++
+    },1000)
+}
+
+
+function renderTime (hours,minutes,seconds,status) {
+    hoursEle.innerText = hours>=10?hours:`0${hours}`;
+    minutesEle.innerText = minutes>=10?minutes:`0${minutes}`;
+    secondsEle.innerText = seconds>=10?seconds:`0${seconds}`;
+    statusEle.innerText =  hours>12?"PM":"AM";
+}
+
+updateTime(hours,minutes,seconds);
